a07c788c0a336f144b4e44e3005789b243238029
Sparakala21 <paraks@rpi.edu>
2024-10-24 11:26:25 -0400

    working multiplayer not working images

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
+const path = require('path'); 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
